9fc89b8b94406863f196c33c97ce44f66c3ad4d1
computershawn <shontek@gmail.com>
2025-02-17 00:35:18 -0800

    Fix avatar and kind of fix tooltip

--- src/components/ui/Sidebar/index.tsx
+++ src/components/ui/Sidebar/index.tsx
@@ -2,7 +2,13 @@
 
 import NextLink from 'next/link';
 
-import { Box, Link as ChakraLink, Text, VStack } from '@chakra-ui/react';
+import {
+  Avatar,
+  Box,
+  Link as ChakraLink,
+  Text,
+  VStack,
+} from '@chakra-ui/react';
 import { AiFillHome } from 'react-icons/ai';
 
 import { Tooltip } from '../tooltip';
@@ -37,11 +43,16 @@ export default function Sidebar() {
       text: 'Create',
       link: '',
     },
-    // {
-    //   icon: <Avatar src='/profilepic.png' />,
-    //   text: 'Profile',
-    //   link: '/computershawn',
-    // },
+    {
+      icon: (
+        <Avatar.Root>
+          <Avatar.Fallback name='Segun Adebayo' />
+          <Avatar.Image src='/profilepic.png' />
+        </Avatar.Root>
+      ),
+      text: 'Profile',
+      link: '/computershawn',
+    },
   ];
 
   return (
@@ -67,12 +78,16 @@ export default function Sidebar() {
           </NextLink>
         </ChakraLink>
         <VStack>
-          {sidebarItems.map((item, index) => (
-            <Tooltip key={item.text} content='This is the tooltip content'>
+          {sidebarItems.map((item) => (
+            <Tooltip
+              key={item.text}
+              content={item.text}
+              // openDelay={500}
+              // closeDelay={100}
+            >
               <Box
-                w={{ base: 10, md: 'full'}}
+                w={{ base: 10, md: 'full' }}
                 display='flex'
-                // align='center'
                 gap={4}
                 _hover={{ bg: 'white' }}
                 borderRadius={6}
@@ -97,6 +112,54 @@ export default function Sidebar() {
                 )}
               </Box>
             </Tooltip>
+
+            // <Tooltip key={item.text} content='This is the tooltip content'>
+            //   <Box
+            //     w={{ base: 10, md: 'full' }}
+            //     display='flex'
+            //     // align='center'
+            //     gap={4}
+            //     _hover={{ bg: 'white' }}
+            //     borderRadius={6}
+            //     p={2}
+            //   >
+            //     {item.link ? (
+            //       <ChakraLink asChild>
+            //         <NextLink href={item.link}>
+            //           {item.icon}
+            //           <Text display={{ base: 'none', md: 'block' }}>
+            //             {item.text}
+            //           </Text>
+            //         </NextLink>
+            //       </ChakraLink>
+            //     ) : (
+            //       <>
+            //         {item.icon}
+            //         <Text display={{ base: 'none', md: 'block' }}>
+            //           {item.text}
+            //         </Text>
+            //       </>
+            //     )}
+            //   </Box>
+            // </Tooltip>
+
+            // <>
+            //   <ChakraTooltip.Root key={item.text}>
+            //     <ChakraTooltip.Trigger asChild>
+            //       {item.text}
+            //     </ChakraTooltip.Trigger>
+            //     {/* <Portal disabled={!portalled} container={portalRef}> */}
+            //     <ChakraTooltip.Positioner>
+            //       <ChakraTooltip.Content>
+            //         <ChakraTooltip.Arrow>
+            //           <ChakraTooltip.ArrowTip />
+            //         </ChakraTooltip.Arrow>
+            //         {'i am a tooltip wassup'}
+            //       </ChakraTooltip.Content>
+            //     </ChakraTooltip.Positioner>
+            //     {/* </Portal> */}
+            //   </ChakraTooltip.Root>
+            // </>
           ))}
         </VStack>
       </VStack>
